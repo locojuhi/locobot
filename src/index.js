bbb76023a77abf992b51b1d72eea765bcc9a71d1
Danny Torres <danny.torresxd@gmail.com>
2020-11-23 01:03:02 -0500

    Bot it's working

--- src/index.js
+++ src/index.js
@@ -5,6 +5,13 @@ config();
 console.log("Bot Running");
 
 const botOptions = {
+    options: { 
+        debug: true 
+    },
+    connection: {
+		secure: true,
+		reconnect: true
+	},
     identity: {
         username: process.env.TWITCH_BOT_USERNAME,
         password: process.env.TWITCH_OAUTH_TOKEN
@@ -14,10 +21,30 @@ const botOptions = {
     ]
 };
 
+console.log(
+    botOptions
+);
 const client = new tmi.client(botOptions);
 
 client.connect();
 
 client.on('connected', (address, port) => {
-    client.action(process.env.TWITCH_BOT_USERNAME, 'Hola jimrsng');
+    console.log('Its Connected?');
+    client.say(process.env.TWITCH_BOT_USERNAME, 'Hola locojuhi');
+});
+
+client.on('chat', (channel, tags, message, self) => {
+    console.log(tags);
+	if(self || !message.startsWith('!')) return;
+
+	const args = message.slice(1).split(' ');
+	const command = args.shift().toLowerCase();
+
+	if(command === 'echo') {
+		client.say(channel, `@${tags.username}, you said: "${args.join(' ')}"`);
+	}
+});
+
+client.on('roomstate', (channel, state) => {
+    console.log(state);
 });
