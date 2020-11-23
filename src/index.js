04d952f29dd4071d30cffd540bb23c1ef85a94ba
Danny Torres <danny.torresxd@gmail.com>
2020-11-22 23:58:42 -0500

    removing dotnev file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
+import { config } from 'dotenv';
+import tmi from 'tmi.js';
+config();
+
 console.log("Bot Running");
+
+const botOptions = {
+    identity: {
+        username: process.env.TWITCH_BOT_USERNAME,
+        password: process.env.TWITCH_OAUTH_TOKEN
+    },
+    channels: [
+        process.env.CHANNEL_NAMES
+    ]
+};
+
+const client = new tmi.client(botOptions);
+
+client.connect();
+
+client.on('connected', (address, port) => {
+    client.action(process.env.TWITCH_BOT_USERNAME, 'Hola jimrsng');
+});
