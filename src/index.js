ad32577ba89292494f33fddc7f4797e27dd4f942
Danny Torres <danny.torresxd@gmail.com>
2020-11-25 01:53:19 -0500

    Removing unnecesary logs

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import { config } from 'dotenv';
 import tmi from 'tmi.js';
-config();
 
-console.log("Bot Running");
+config();
 
 const botOptions = {
     options: { 
@@ -21,20 +20,11 @@ const botOptions = {
     ]
 };
 
-console.log(
-    botOptions
-);
-const client = new tmi.client(botOptions);
 
+const client = new tmi.client(botOptions)
 client.connect();
 
-client.on('connected', (address, port) => {
-    console.log('Its Connected?');
-    client.say(process.env.TWITCH_BOT_USERNAME, 'Hola locojuhi');
-});
-
 client.on('chat', (channel, tags, message, self) => {
-    console.log(tags);
 	if(self || !message.startsWith('!')) return;
 
 	const args = message.slice(1).split(' ');
@@ -43,8 +33,4 @@ client.on('chat', (channel, tags, message, self) => {
 	if(command === 'echo') {
 		client.say(channel, `@${tags.username}, you said: "${args.join(' ')}"`);
 	}
-});
-
-client.on('roomstate', (channel, state) => {
-    console.log(state);
 });
